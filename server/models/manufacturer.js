6616d6609df5200e52d9f87115f0aade7ef37388
Kyrylo <fresh.js.ftd.@gmail.com>
2024-04-02 19:02:45 +0300

    Add custom validation example

--- server/models/manufacturer.js
+++ server/models/manufacturer.js
@@ -1,32 +1,44 @@
 const { Schema, model } = require('mongoose');
+const yup = require('yup');
+
+const EMAIL_VALIDATION_SCHEMA = yup.string().required().email();
 
 const manufacturerSchema = new Schema({
   name: {
     type: String,
     required: true,
     match: /[A-Za-z0-9. ]+/,
-    unique: true
+    unique: true,
   },
   // name: Schema.Types.String
   estimatedValue: {
     type: Number,
     min: 0,
-    max: 9999999
+    max: 9999999,
   },
   isSubsidiary: {
     type: Boolean,
   },
   creationDate: {
     type: Date,
-    min: new Date(0,1,1),
+    min: new Date(0, 1, 1),
   },
   address: {
     country: {
-      type: String, 
-      enum : ['UA', 'Ukraine', 'UK', 'GB', 'SWE', 'IT']
+      type: String,
+      enum: ['UA', 'Ukraine', 'UK', 'GB', 'SWE', 'IT'],
     },
     city: String,
     postalCode: Number,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: async (emailValue) =>
+          EMAIL_VALIDATION_SCHEMA.isValid(emailValue),
+      },
+    },
   },
 });
 
