b2e7b4d6ea1ae84284097f16a1cf5d68f4ee359e
Kyrylo <fresh.js.ftd.@gmail.com>
2024-04-04 19:18:54 +0300

    Add products read and update routes and controller handlers

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,4 +1,5 @@
-const { Product } = require('../models');
+const createHttpError = require('http-errors');
+const { Product, Manufacturer } = require('../models');
 
 module.exports.createProduct = async (req, res, next) => {
   try {
@@ -18,7 +19,19 @@ module.exports.createProduct = async (req, res, next) => {
     // додає айді до масиву якщо тай його немає
     // await manufacturer.updateOne({ $addToSet: { products: product._id }});
 
-    res.status(201).send({data: product});
+    res.status(201).send({ data: product });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find()
+      .populate('manufacturer')
+      .select('-__v');
+
+    res.status(200).send({ data: products });
   } catch (error) {
     next(error);
   }
@@ -29,7 +42,73 @@ module.exports.getProducts = async (req, res, next) => {
     
     const products = await Product.find().populate('manufacturer').select('-__v');
 
-    res.status(200).send({data: products});
+    res.status(200).send({ data: products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getProduct = async (req, res, next) => {
+  try {
+    const {
+      params: { productId },
+    } = req;
+
+    const product = await Product.findById(productId)
+      .populate({ path: 'manufacturer', select: ['-products', '-__v'] })
+      .select('-__v');
+
+    if (!product) {
+      throw createHttpError(404, 'Product not found');
+    }
+
+    res.send({ data: product });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateProduct = async (req, res, next) => {
+  try {
+    const {
+      params: { productId },
+      body,
+    } = req;
+
+    // 1. якщо ми намагаємося оновити виробника то маємо перевірити. чи він існує
+    if (body.manufacturer) {
+      const manufacturer = await Manufacturer.findById(body.manufacturer);
+
+      if (!manufacturer) {
+        throw createHttpError(404, 'Manufacturer not found');
+      }
+    }
+
+    // 2. оновити продукт
+    let product = await Product.findByIdAndUpdate(productId, body);
+
+    if (!product) {
+      throw createHttpError(404, 'Product not found');
+    }
+
+    // 3. якщо ми оновили виробника то треба
+    if (body.manufacturer) {
+      // 3.1 видалити id товара у старого виробника
+      await Manufacturer.updateOne( { _id: product.manufacturer },{
+        $pull: { products: product._id }
+      });
+
+      // 3.2 додати новому виробнику id товара
+      await Manufacturer.updateOne(
+        { _id: body.manufacturer },
+        { $addToSet: { products: product._id } }
+      );
+    }
+
+    // 4. повернути оновлені дані про продукт
+    product = await Product.findById(productId).select('-__v');
+
+    res.send({ data: product });
   } catch (error) {
     next(error);
   }
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,12 @@
 const rootRouter = require('express').Router();
 const manufacturersRouter = require('./manufacturersRouter');
+const ProductsController = require('../controllers/products.controller');
 
 rootRouter.use('/manufacturers', manufacturersRouter);
 
+rootRouter.get('/products', ProductsController.getAllProducts);
+
+rootRouter.route('/products/:productId')
+  .get(ProductsController.getProduct)
+  .put(ProductsController.updateProduct);
 module.exports = rootRouter;
